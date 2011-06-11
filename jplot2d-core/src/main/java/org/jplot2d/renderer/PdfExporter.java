fd75e70aee22ac81602557ea58ef0908ba87aba4
ravennix <jplot2d@users.noreply.github.com>
2011-06-11 16:53:58 +0800

    add PdfExporter PngFileExporter PngStreamExporter

---
+++ jplot2d-core/src/main/java/org/jplot2d/renderer/PdfExporter.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2010, 2011 Jingjing Li.
+ *
+ * This file is part of jplot2d.
+ *
+ * jplot2d is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * jplot2d is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jplot2d. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jplot2d.renderer;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Map;
+
+import org.jplot2d.element.impl.ComponentEx;
+import org.jplot2d.element.impl.PlotEx;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfWriter;
+
+/**
+ * Export plot image to a png file.
+ * 
+ * @author Jingjing Li
+ * 
+ */
+public class PdfExporter extends Renderer<File> {
+
+	private final OutputStream os;
+
+	private String title;
+
+	public PdfExporter(String pathname) throws FileNotFoundException {
+		this(new File(pathname));
+	}
+
+	public PdfExporter(File file) throws FileNotFoundException {
+		this(new FileOutputStream(file));
+	}
+
+	public PdfExporter(OutputStream out) {
+		super();
+		os = new BufferedOutputStream(out);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	@Override
+	public void render(PlotEx plot,
+			Map<ComponentEx, ComponentEx> cacheableCompMap,
+			Collection<ComponentEx> unmodifiedCacheableComps,
+			Map<ComponentEx, ComponentEx[]> subcompsMap) {
+
+		Dimension size = getDeviceBounds(plot).getSize();
+
+		Document document = new Document(new com.itextpdf.text.Rectangle(
+				size.width, size.height), 0, 0, 0, 0);
+		PdfWriter writer = null;
+		try {
+			writer = PdfWriter.getInstance(document, os);
+		} catch (DocumentException e) {
+			/*
+			 * should not happen but if it happens it should be notified to the
+			 * integration instead of leaving it half-done and tell nothing.
+			 */
+			throw new RuntimeException("Error creating PDF document", e);
+		}
+
+		document.open();
+		if (title != null) {
+			document.addTitle(title);
+		}
+		PdfContentByte cb = writer.getDirectContent();
+		Graphics2D g = cb.createGraphicsShapes(size.width, size.height);
+
+		for (Map.Entry<ComponentEx, ComponentEx> me : cacheableCompMap
+				.entrySet()) {
+			ComponentEx ccopy = me.getValue();
+
+			ComponentEx[] sublist = subcompsMap.get(ccopy);
+			for (ComponentEx subcomp : sublist) {
+				subcomp.draw(g);
+			}
+		}
+
+		g.dispose();
+		document.close();
+	}
+
+}
---
+++ jplot2d-core/src/main/java/org/jplot2d/renderer/PngFileExporter.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2010, 2011 Jingjing Li.
+ *
+ * This file is part of jplot2d.
+ *
+ * jplot2d is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * jplot2d is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jplot2d. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jplot2d.renderer;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+/**
+ * Export plot image to a png file.
+ * 
+ * @author Jingjing Li
+ * 
+ */
+public class PngFileExporter extends ImageRenderer {
+
+	private static final ImageFactory INT_RGB_IMAGEFACTORY = new BufferedImageFactory(
+			BufferedImage.TYPE_INT_RGB, Color.WHITE);
+
+	public PngFileExporter(String pathname) {
+		this(new File(pathname));
+	}
+
+	public PngFileExporter(File file) {
+		this(INT_RGB_IMAGEFACTORY, file);
+	}
+
+	public PngFileExporter(ImageFactory imageFactory, final File file) {
+		super(imageFactory);
+
+		addRenderingFinishedListener(new RenderingFinishedListener() {
+
+			public void renderingFinished(RenderingFinishedEvent event) {
+				try {
+					ImageIO.write((RenderedImage) event.getResult(), "PNG",
+							file);
+				} catch (IOException e) {
+					throw new RuntimeException("Png file I/O exception", e);
+				}
+
+			}
+		});
+	}
+
+}
