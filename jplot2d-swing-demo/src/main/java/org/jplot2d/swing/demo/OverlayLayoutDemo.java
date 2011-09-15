d2e6d6ab68115938c4560fa7a0f6e33ef3ae91cc
ravennix <ravennix@localhost>
2011-09-15 13:53:45 +0800

    finish py demo for Overlay layout

--- jplot2d-swing-demo/src/main/java/org/jplot2d/swing/demo/OverlayLayoutDemo.java
+++ jplot2d-swing-demo/src/main/java/org/jplot2d/swing/demo/OverlayLayoutDemo.java
@@ -18,15 +18,12 @@
  */
 package org.jplot2d.swing.demo;
 
-import java.io.IOException;
-
 import javax.swing.JFrame;
 
 import org.jplot2d.element.ElementFactory;
 import org.jplot2d.element.Axis;
 import org.jplot2d.element.Layer;
 import org.jplot2d.element.Plot;
-import org.jplot2d.env.RenderEnvironment;
 import org.jplot2d.layout.BoundsConstraint;
 import org.jplot2d.layout.OverlayLayoutDirector;
 import org.jplot2d.sizing.FillContainerSizeMode;
@@ -43,7 +40,10 @@ public class OverlayLayoutDemo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Plot plot = ElementFactory.getInstance().createPlot();
+
+		ElementFactory ef = ElementFactory.getInstance();
+
+		Plot plot = ef.createPlot();
 		plot.setSizeMode(new FillContainerSizeMode(1));
 		plot.setLayoutDirector(new OverlayLayoutDirector());
 
@@ -51,27 +51,27 @@ public class OverlayLayoutDemo {
 		frame.setSize(640, 480);
 		frame.setVisible(true);
 
-		Axis[] xaxes = ElementFactory.getInstance().createAxes(2);
-		Axis[] yaxes = ElementFactory.getInstance().createAxes(2);
+		Axis[] xaxes = ef.createAxes(2);
+		Axis[] yaxes = ef.createAxes(2);
 		xaxes[0].getTitle().setText("x axis");
 		yaxes[0].getTitle().setText("y axis");
 		plot.addXAxes(xaxes);
 		plot.addYAxes(yaxes);
 
-		Layer layer = ElementFactory.getInstance().createLayer(new double[] { 0, 0.1, 0.2 },
-				new double[] { 0, 0.1, 0.4 }, "line A");
+		Layer layer = ef.createLayer(new double[] { 0, 0.1, 0.2 }, new double[] { 0, 0.1, 0.4 },
+				"line A");
 		plot.addLayer(layer, xaxes[0], yaxes[0]);
 
-		Plot sp1 = ElementFactory.getInstance().createSubplot();
-		Layer nestLayer = ElementFactory.getInstance().createLayer(new double[] { 0, 0.1, 0.2 },
+		Plot sp1 = ef.createSubplot();
+		Layer nestLayer = ef.createLayer(new double[] { 0, 0.1, 0.2 },
 				new double[] { 0, 0.1, 0.4 }, "line B");
 
 		plot.addSubplot(sp1, new BoundsConstraint(new Insets2D(0, 0, 0, 0), new Insets2D(0.05,
 				0.05, 0.45, 0.45)));
 		sp1.setLocation(80, 250);
 		sp1.setSize(300, 200);
-		Axis[] p1x = ElementFactory.getInstance().createAxes(2);
-		Axis[] p1y = ElementFactory.getInstance().createAxes(2);
+		Axis[] p1x = ef.createAxes(2);
+		Axis[] p1y = ef.createAxes(2);
 		p1x[0].getTitle().setText("x axis");
 		p1y[0].getTitle().setText("y axis");
 		p1x[1].setTickVisible(false);
@@ -82,12 +82,6 @@ public class OverlayLayoutDemo {
 		sp1.addYAxes(p1y);
 		sp1.addLayer(nestLayer, p1x[0], p1y[0]);
 
-		try {
-			((RenderEnvironment) plot.getEnvironment()).saveAsPNG("OverlayLayoutDemo.png");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 
 }
