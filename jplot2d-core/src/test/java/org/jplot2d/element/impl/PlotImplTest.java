2456c59f39dbee83fdec79a6063ef29eb6504e8c
ravennix <ravennix@localhost>
2011-10-05 08:41:27 +0800

    improve test case for copyStructor

--- jplot2d-core/src/test/java/org/jplot2d/element/impl/PlotImplTest.java
+++ jplot2d-core/src/test/java/org/jplot2d/element/impl/PlotImplTest.java
@@ -359,6 +359,11 @@ public class PlotImplTest {
 		assertSame(p2, p2.getYAxis(0).getParent());
 		assertSame(p2, p2.getLayer(0).getParent());
 		assertSame(p2.getLayer(0), p2.getLayer(0).getGraphPlotter(0).getParent());
+		// check link
+		assertSame(p2.getLayer(0).getXRangeManager(), p2.getXAxis(0).getTickManager()
+				.getRangeManager());
+		assertSame(p2.getLayer(0).getYRangeManager(), p2.getYAxis(0).getTickManager()
+				.getRangeManager());
 
 	}
 
