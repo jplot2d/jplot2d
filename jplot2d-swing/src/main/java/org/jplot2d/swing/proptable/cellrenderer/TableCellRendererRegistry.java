414f6a780970745c58a489696084855776f61d69
ravennix <jplot2d@users.noreply.github.com>
2011-12-01 14:39:39 +0800

    property table cell renderer: cut off round error when show float and double values

--- jplot2d-swing/src/main/java/org/jplot2d/swing/proptable/cellrenderer/TableCellRendererRegistry.java
+++ jplot2d-swing/src/main/java/org/jplot2d/swing/proptable/cellrenderer/TableCellRendererRegistry.java
@@ -1,23 +1,20 @@
-/*
- * This file is part of Herschel Common Science System (HCSS).
- * Copyright 2001-2010 Herschel Science Ground Segment Consortium
- *
- * HCSS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * HCSS is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General
- * Public License along with HCSS.
- * If not, see <http://www.gnu.org/licenses/>.
- */
 /**
- * 
+ * Copyright 2010, 2011 Jingjing Li.
+ *
+ * This file is part of jplot2d.
+ *
+ * jplot2d is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * jplot2d is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jplot2d. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jplot2d.swing.proptable.cellrenderer;
 
@@ -82,6 +79,12 @@ public class TableCellRendererRegistry implements TableCellRendererFactory {
 	public void registerDefaults() {
 		typeRendererMap.clear();
 
+		// number renderer
+		registerRenderer(float.class, new FloatCellRenderer());
+		registerRenderer(Float.class, new FloatCellRenderer());
+		registerRenderer(double.class, new DoubleCellRenderer());
+		registerRenderer(Double.class, new DoubleCellRenderer());
+		
 		// geom renderer
 		registerRenderer(Point.class, new PointCellRenderer());
 		registerRenderer(Point2D.class, new Point2DCellRenderer());
