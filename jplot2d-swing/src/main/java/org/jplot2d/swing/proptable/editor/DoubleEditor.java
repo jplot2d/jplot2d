a1529ec621ec4794bfa57e3b95779af81e675be3
ravennix <jplot2d@users.noreply.github.com>
2011-12-05 16:57:48 +0800

    Float and Double editor using Locale.US new FloatArrayEditor and DoubleArrayEditor

--- jplot2d-swing/src/main/java/org/jplot2d/swing/proptable/editor/DoubleEditor.java
+++ jplot2d-swing/src/main/java/org/jplot2d/swing/proptable/editor/DoubleEditor.java
@@ -1,24 +1,25 @@
-/*
- * This file is part of Herschel Common Science System (HCSS).
- * Copyright 2001-2010 Herschel Science Ground Segment Consortium
+/**
+ * Copyright 2010, 2011 Jingjing Li.
  *
- * HCSS is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * This file is part of jplot2d.
  *
- * HCSS is distributed in the hope that it will be useful,
+ * jplot2d is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * jplot2d is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Lesser Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General
- * Public License along with HCSS.
- * If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jplot2d. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jplot2d.swing.proptable.editor;
 
 import java.text.NumberFormat;
+import java.util.Locale;
 
 /**
  * 
@@ -26,22 +27,22 @@ import java.text.NumberFormat;
  */
 public class DoubleEditor extends FormattedEditor {
 
-    private static NumberFormat getDoubleFormat() {
-        NumberFormat format = NumberFormat.getNumberInstance();
-        format.setMaximumFractionDigits(16);
-        return format;
-    }
+	private static NumberFormat getDoubleFormat() {
+		NumberFormat format = NumberFormat.getNumberInstance(Locale.US);
+		format.setMaximumFractionDigits(16);
+		return format;
+	}
 
-    public DoubleEditor() {
-        super(getDoubleFormat());
-    }
+	public DoubleEditor() {
+		super(getDoubleFormat());
+	}
 
-    @Override
-    public Object getValue() {
-        Object v = super.getValue();
-        if (v instanceof Number) {
-            return new Double(((Number) v).doubleValue());
-        }
-        return v;
-    }
+	@Override
+	public Object getValue() {
+		Object v = super.getValue();
+		if (v instanceof Number) {
+			return new Double(((Number) v).doubleValue());
+		}
+		return v;
+	}
 }
